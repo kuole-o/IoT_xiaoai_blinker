d191f9464834ee55451841b3414f7e861948d507
guole <guole.fun@qq.com>
2024-12-01 01:17:47 +0800

    优化代码结构

--- xiaoai_4_no_off_BlinkerButton5/xiaoai_4_on_off_BlinkerButton5.ino
+++ xiaoai_4_no_off_BlinkerButton5/xiaoai_4_on_off_BlinkerButton5.ino
@@ -1,23 +1,4 @@
 /* *****************************************************************
-   Download latest Blinker library here:
-   https://github.com/blinker-iot/blinker-library/archive/master.zip
-
-   Blinker is a cross-hardware, cross-platform solution for the IoT.
-   It provides APP, device and server support,
-   and uses public cloud services for data transmission and storage.
-   It can be used in smart home, data monitoring and other fields
-   to help users build Internet of Things projects better and faster.
-
-   Make sure installed 2.7.4 or later ESP8266/Arduino package,
-   if use ESP8266 with Blinker.
-   https://github.com/esp8266/Arduino/releases
-
-   Make sure installed 1.0.4 or later ESP32/Arduino package,
-   if use ESP32 with Blinker.
-   https://github.com/espressif/arduino-esp32/releases
-
-   Docs: https://diandeng.tech/doc
- * *****************************************************************
    Blinker 库下载地址:
    https://github.com/blinker-iot/blinker-library/archive/master.zip
 
@@ -37,16 +18,22 @@
  * *****************************************************************/
 
 #define BLINKER_WIFI
-#define BLINKER_MIOT_MULTI_OUTLET    //设置为小爱多个插座的模式
+#define BLINKER_MIOT_MULTI_OUTLET  //设置为小爱多个插座的模式
 
 #include <Blinker.h>
 
-char i;
-char auth[] = "******";
-char ssid[] = "******";
-char pswd[] = "******";
+// 使用宏定义或常量表示引脚编号，增强可读性和可维护性
+const int PIN_YAO_TOU_ON_OFF = 13;         //摇头/不摇头
+const int PIN_LOW_TEMP_WEAK_WIND = 12;     //低温/弱风
+const int PIN_HIGH_TEMP_STRONG_WIND = 27;  //高温/强风
+const int PIN_UNDEFINED = 25;              //未定义
 
-bool oState[5] = { false };
+char auth[] = "********";  //点灯 App 申请的 Secret Key
+char ssid[] = "********";
+char pswd[] = "********";
+
+// 电源状态
+bool outletPowerStates[5] = { false };
 
 // 新建组件对象，要和APP组件’数据键名’一致
 BlinkerButton Button1("btn_a");
@@ -55,205 +42,154 @@ BlinkerButton Button3("btn_c");
 BlinkerButton Button4("btn_d");
 BlinkerButton Button5("btn_all");
 
+// 用于记录Button1、Button2、Button3状态的全局变量
+bool button1CurrentState = false;
+bool button2CurrentState = false;
+bool button3CurrentState = false;
+
+// 封装设置引脚状态并更新按钮相关信息的函数
+void setPinAndButtonState(int pin, BlinkerButton &button, const String &state, const String &text) {
+  BLINKER_LOG("当前按键预期 state: ", state);
+
+  digitalWrite(pin, state == "on" ? HIGH : LOW);
+  button.print(state);
+  button.text(text);
+}
+
 // 按下Blinker APP按键1即会执行该函数
-void button1_callback(const String & state)
-{
-  BLINKER_LOG("get button state: ", state);
-  digitalWrite(13, !digitalRead(13));
-  if (state == "on") {
-    digitalWrite(13, LOW);
-    Button1.print("on");
-    Button1.text("摇头：开");
-  }
-  else if (state == "off") {
-    digitalWrite(13, HIGH);
-    Button1.print("off");
-    Button1.text("摇头：关");
+void button1_callback(const String &state) {
+  BLINKER_LOG("get button1 state: ", state);
+
+  if (state == "off") {
+    setPinAndButtonState(PIN_YAO_TOU_ON_OFF, Button1, "off", "摇头");
+  } else if (state == "on") {
+    setPinAndButtonState(PIN_YAO_TOU_ON_OFF, Button1, "on", "摇头");
   }
 }
 
 // 按下Blinker APP按键2即会执行该函数
-void button2_callback(const String & state)
-{
-  BLINKER_LOG("get button state: ", state);
-  digitalWrite(12, !digitalRead(12));
-  if (state == "on") {
-    digitalWrite(27, LOW);
-    digitalWrite(12, HIGH);
-    Button3.print("off");
-    Button3.text("高温/强风");
-    Button2.print("on");
-    Button2.text("低温/弱风");
-  }
-  else if (state == "off") {
-    digitalWrite(12, LOW);
-    Button2.print("off");
-    Button2.text("低温/弱风");
+void button2_callback(const String &state) {
+  BLINKER_LOG("get button2 state: ", state);
+
+  if (state == "off") {
+    setPinAndButtonState(PIN_LOW_TEMP_WEAK_WIND, Button2, "off", "低温/弱风");
+  } else if (state == "on") {
+    setPinAndButtonState(PIN_LOW_TEMP_WEAK_WIND, Button2, "on", "低温/弱风");
+    setPinAndButtonState(PIN_HIGH_TEMP_STRONG_WIND, Button3, "off", "高温/强风");
   }
 }
 
 // 按下Blinker APP按键3即会执行该函数
-void button3_callback(const String & state)
-{
-  BLINKER_LOG("get button state: ", state);
-  digitalWrite(27, !digitalRead(27));
-  if (state == "on") {
-    digitalWrite(12, LOW);
-    digitalWrite(27, HIGH);
-    Button2.print("off");
-    Button2.text("低温/弱风");
-    Button3.print("on");
-    Button3.text("高温/强风");
-  }
-  else if (state == "off") {
-    digitalWrite(27, LOW);
-    Button3.print("off");
-    Button3.text("高温/强风");
+void button3_callback(const String &state) {
+  BLINKER_LOG("get button3 state: ", state);
+
+  if (state == "off") {
+    setPinAndButtonState(PIN_HIGH_TEMP_STRONG_WIND, Button3, "off", "高温/强风");
+  } else if (state == "on") {
+    setPinAndButtonState(PIN_LOW_TEMP_WEAK_WIND, Button2, "off", "低温/弱风");
+    setPinAndButtonState(PIN_HIGH_TEMP_STRONG_WIND, Button3, "on", "高温/强风");
   }
 }
 
 // 按下Blinker APP按键4即会执行该函数
-void button4_callback(const String & state)
-{
-  BLINKER_LOG("get button state: ", state);
-  digitalWrite(25, !digitalRead(25));
-  if (state == "on") {
-    digitalWrite(25, HIGH);
-    Button4.print("on");
-    Button4.text("未定义：开");
-  }
-  else if (state == "off") {
-    digitalWrite(25, LOW);
-    Button4.print("off");
-    Button4.text("未定义：关");
+void button4_callback(const String &state) {
+  BLINKER_LOG("get button4 state: ", state);
+
+  if (state == "off") {
+    setPinAndButtonState(PIN_UNDEFINED, Button4, "off", "电蚊香");
+  } else if (state == "on") {
+    setPinAndButtonState(PIN_UNDEFINED, Button4, "on", "电蚊香");
   }
 }
 
 // 按下Blinker APP按键5即会执行该函数
-void button5_callback(const String & state)
-{
+void button5_callback(const String &state) {
   BLINKER_LOG("get button state: ", state);
-  digitalWrite(13, !digitalRead(13));
-  digitalWrite(12, !digitalRead(12));
-  if (state == "on") {
-    digitalWrite(13, HIGH);
-    digitalWrite(12, HIGH);
-    Button1.print("on");
-    Button2.print("on");
-    Button1.text("摇头：开");
-    Button2.text("低温/弱风");
-  }
-  else if (state == "off") {
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    digitalWrite(27, LOW);
-    Button1.print("off");
-    Button2.print("off");
-    Button3.print("off");
-    Button1.text("摇头：关");
-    Button2.text("低温/弱风");
-    Button3.text("高温/强风");
+
+  if (!digitalRead(PIN_YAO_TOU_ON_OFF) && !digitalRead(PIN_LOW_TEMP_WEAK_WIND) && !digitalRead(PIN_HIGH_TEMP_STRONG_WIND)) {
+    setPinAndButtonState(PIN_YAO_TOU_ON_OFF, Button1, "on", "摇头");
+    setPinAndButtonState(PIN_LOW_TEMP_WEAK_WIND, Button2, "on", "低温/弱风");
+  } else if {
+    setPinAndButtonState(PIN_YAO_TOU_ON_OFF, Button1, "off", "摇头");
+    setPinAndButtonState(PIN_LOW_TEMP_WEAK_WIND, Button2, "off", "低温/弱风");
+    setPinAndButtonState(PIN_HIGH_TEMP_STRONG_WIND, Button3, "off", "低温/弱风");
   }
 }
 
 // 13-摇头/不摇头；12-低温/弱风；27-高温/强风
-void miotPowerState(const String & state, uint8_t num)
-{
+void miotPowerState(const String &state, uint8_t num) {
   BLINKER_LOG("need set outlet: ", num, ", power state: ", state);
-  i = 0; //初始化
 
-  if (num == 1) {
-    i = 13;
-  }
-  else if (num == 2) {
-    i = 12;
-  }
-  else if (num == 3) {
-    i = 27;
-  }
-  else if (num == 4) {
-    i = 25;
+  int pinIndex;
+  switch (num) {
+    case 1:
+      pinIndex = PIN_YAO_TOU_ON_OFF;
+      break;
+    case 2:
+      pinIndex = PIN_LOW_TEMP_WEAK_WIND;
+      break;
+    case 3:
+      pinIndex = PIN_HIGH_TEMP_STRONG_WIND;
+      break;
+    case 4:
+      pinIndex = PIN_UNDEFINED;
+      break;
   }
 
-  if (state == BLINKER_CMD_ON) {
-    switch (i) {
-      case 12:
-        digitalWrite(27, LOW);
-        digitalWrite(i, !digitalRead(27));
-        BlinkerMIOT.powerState("on", num);
-        BlinkerMIOT.print();
-        oState[num] = true;
-        break;
-      case 27:
-        digitalWrite(12, LOW);
-        digitalWrite(i, !digitalRead(12));
-        BlinkerMIOT.powerState("on", num);
-        BlinkerMIOT.print();
-        oState[num] = true;
-        break;
-      default:
-        digitalWrite(i, HIGH);
-        BlinkerMIOT.powerState("on", num);
-        BlinkerMIOT.print();
-        oState[num] = true;
+  if (state == BLINKER_CMD_OFF) {
+    if (num == 2) {
+      digitalWrite(PIN_HIGH_TEMP_STRONG_WIND, LOW);
+    } else if (num == 3) {
+      digitalWrite(PIN_LOW_TEMP_WEAK_WIND, LOW);
     }
+    digitalWrite(pinIndex, HIGH);
+    BlinkerMIOT.powerState("on");
+    BlinkerMIOT.print();
+    outletPowerStates[num] = true;
+  } else if (state == BLINKER_CMD_ON) {
+    digitalWrite(pinIndex, LOW);
+    BlinkerMIOT.powerState("off");
+    BlinkerMIOT.print();
+    outletPowerStates[num] = false;
   }
-  else if (state == BLINKER_CMD_OFF) {
-    switch (i) {
-      case 12:
-        digitalWrite(27, LOW);
-        digitalWrite(i, LOW);
-        BlinkerMIOT.powerState("off", num);
-        BlinkerMIOT.print();
-        oState[num] = false;
-        break;
-      case 27:
-        digitalWrite(12, LOW);
-        digitalWrite(i, LOW);
-        BlinkerMIOT.powerState("off", num);
-        BlinkerMIOT.print();
-        oState[num] = false;
-        break;
-      default:
-        digitalWrite(i, LOW);
-        BlinkerMIOT.powerState("off", num);
-        BlinkerMIOT.print();
-        oState[num] = false;
-    }
 
-    if (num == 0) {
-      for (uint8_t o_num = 0; o_num < 5; o_num++) {
-        oState[o_num] = false;
-      }
+  // 处理全部插座关闭的情况
+  if (num == 0 && state == "off") {
+    for (uint8_t o_num = 0; o_num < 5; o_num++) {
+      outletPowerStates[o_num] = false;
     }
   }
 }
 
-void miotQuery(int32_t queryCode, uint8_t num)
-{
-  BLINKER_LOG("AliGenie Query outlet: ", num, ", codes: ", queryCode);
+// 添加错误处理逻辑到查询函数中
+void miotQuery(int32_t queryCode, uint8_t num) {
+  BLINKER_LOG("小爱语控指令: ", num, ", codes: ", queryCode);
 
-  switch (queryCode)
-  {
-    case BLINKER_CMD_QUERY_ALL_NUMBER :
+  // 先进行数据一致性检查，确保状态值有效
+  if (num < 0 || num >= 5) {
+    BLINKER_LOG("Invalid outlet number in query!");
+    return;
+  }
+
+  switch (queryCode) {
+    case BLINKER_CMD_QUERY_ALL_NUMBER:
       BLINKER_LOG("MIOT Query All");
-      BlinkerMIOT.powerState(oState[num] ? "on" : "off", num);
+      BlinkerMIOT.powerState(outletPowerStates[num] ? "on" : "off", num);
       BlinkerMIOT.print();
       break;
-    case BLINKER_CMD_QUERY_POWERSTATE_NUMBER :
+    case BLINKER_CMD_QUERY_POWERSTATE_NUMBER:
       BLINKER_LOG("MIOT Query Power State");
-      BlinkerMIOT.powerState(oState[num] ? "on" : "off", num);
+      BlinkerMIOT.powerState(outletPowerStates[num] ? "on" : "off", num);
       BlinkerMIOT.print();
       break;
-    default :
-      BlinkerMIOT.powerState(oState[num] ? "on" : "off", num);
+    default:
+      BlinkerMIOT.powerState(outletPowerStates[num] ? "on" : "off", num);
       BlinkerMIOT.print();
       break;
   }
 }
 
-void dataRead(const String & data)
-{
+void dataRead(const String &data) {
   BLINKER_LOG("Blinker readString: ", data);
 
   Blinker.vibrate();
@@ -263,19 +199,23 @@ void dataRead(const String & data)
   Blinker.print("millis", BlinkerTime);
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   BLINKER_DEBUG.stream(Serial);
+  //BLINKER_DEBUG.debugAll();
 
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-  pinMode(12, OUTPUT);
-  digitalWrite(12, LOW);
-  pinMode(27, OUTPUT);
-  digitalWrite(27, LOW);
-  pinMode(25, OUTPUT);
-  digitalWrite(25, LOW);
+  // 使用宏定义的引脚编号设置引脚模式和初始电平
+  pinMode(PIN_YAO_TOU_ON_OFF, OUTPUT);
+  digitalWrite(PIN_YAO_TOU_ON_OFF, LOW);
+
+  pinMode(PIN_LOW_TEMP_WEAK_WIND, OUTPUT);
+  digitalWrite(PIN_LOW_TEMP_WEAK_WIND, LOW);
+
+  pinMode(PIN_HIGH_TEMP_STRONG_WIND, OUTPUT);
+  digitalWrite(PIN_HIGH_TEMP_STRONG_WIND, LOW);
+
+  pinMode(PIN_UNDEFINED, OUTPUT);
+  digitalWrite(PIN_UNDEFINED, LOW);
 
   Blinker.begin(auth, ssid, pswd);
   Blinker.attachData(dataRead);
@@ -289,7 +229,6 @@ void setup()
   BlinkerMIOT.attachQuery(miotQuery);
 }
 
-void loop()
-{
+void loop() {
   Blinker.run();
 }
